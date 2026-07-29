3b33de175dd9bc23e9ca20d47b98d113c2582148
Fahad-smto <simanto.smto.prog@gmail.com>
2026-07-30 01:30:48 +0600

    pages added register and login pages code added also added shadcn

---
+++ app/(dashboardGroup)/adminDashboard/page.tsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <h2>AdminDashboard</h2>
+    </div>
+  );
+};
+
+export default memo(AdminDashboard);
---
+++ app/(dashboardGroup)/customerDashboard/page.tsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+
+const CustomerDashboard = () => {
+  return (
+    <div>
+      <h2>CustomerDashboard</h2>
+    </div>
+  );
+};
+
+export default memo(CustomerDashboard);
